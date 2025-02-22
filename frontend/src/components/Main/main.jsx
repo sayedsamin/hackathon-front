7a8c4d1d8476ad56d3e90127368b02bc8a32e5b8
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2025-02-22 09:44:51 -0600

    Added dynamic source to the animation

--- frontend/src/components/Main/main.jsx
+++ frontend/src/components/Main/main.jsx
@@ -6,10 +6,38 @@ import "./main.css";
 
 const Main = () => {
   const [habbitData, setHabbitData] = useState(sustainabilityData);
+  const [checkedCount, setCheckedCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+
+  useEffect(() => {
+    const total = habbitData.reduce(
+      (acc, category) => acc + category.subCategories.length,
+      0
+    );
+    const checked = habbitData.reduce(
+      (acc, category) =>
+        acc + category.subCategories.filter((sub) => sub.checked).length,
+      0
+    );
+
+    setTotalCount(total);
+    setCheckedCount(checked);
+  }, [habbitData]);
+
+  const markedPercent = (checkedCount / totalCount) * 100;
+
+  let source = null;
+
+  if (markedPercent < 50)
+    source =
+      "https://lottie.host/99a1a143-2e4f-4ff1-a182-2a47eb705005/o1hsE75UgZ.lottie";
+  else
+    source =
+      "https://lottie.host/8a80387c-848e-4178-bcf7-260931c8b3d7/Qv6k5WRM7c.lottie";
 
   return (
     <main style={{ padding: "2rem", minHeight: "90vh" }}>
-      <LottieAnimation src="https://lottie.host/99a1a143-2e4f-4ff1-a182-2a47eb705005/o1hsE75UgZ.lottie" />
+      <LottieAnimation src={source} />
       <div className="dashboard">
         {habbitData.map(({ id, mainCategory, subCategories }) => (
           <Card
